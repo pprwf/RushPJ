058ce7f1fcdf38e4e900d8db01a6273dc4dad894
pprwf <pprwfwithsleeping.pills@gmail.com>
2023-12-21 13:37:04 +0700

    feat: include tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./website/*.html"],
+  content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {},
   },
   plugins: [],
 }
-
