413acd02ac72644f2b361fa023eee0a579b52813
Rainnie-007 <rainandrain3@gmail.com>
2023-12-22 23:25:21 +0700

    Push

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./website/*.html"],
+  content: ["./website/**/*.html"],
   theme: {
+    
     extend: {},
   },
   plugins: [],
